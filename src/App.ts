c157fae6f6f89cda86bb125b5a76471ee5225be4
Tristan Tarpley <tristan@marketr.life>
2020-05-26 16:25:46 -0500

    app.listen

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 export const app = express()
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('listening')
 })
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
