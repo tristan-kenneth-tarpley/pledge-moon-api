c1927f2f2fe703783ffac9df13cb17d63ec41a3b
Tristan Tarpley <tristan@marketr.life>
2020-05-26 16:32:25 -0500

    app.ts

--- src/App.ts
+++ src/App.ts
@@ -3,7 +3,7 @@ export const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-    res.send('listening!!')
+    res.send('listening!!!')
 })
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
